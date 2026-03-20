ad947004ae0be2840878d3373ee8ac89eb7c7890
Red-Ducky <galaxy2yt2.0@gmail.com>
2026-03-20 19:05:34 +0100

    Update payload.ps1

--- Popups/payload.ps1
+++ Popups/payload.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
 while ($true) {
     [System.Windows.Forms.MessageBox]::Show(
-        "Coucou Mathéo !",
+        "Coucou Matheo !",
         "Message",
         [System.Windows.Forms.MessageBoxButtons]::OK,
         [System.Windows.Forms.MessageBoxIcon]::Information
