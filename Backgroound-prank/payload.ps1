d06972edd5f15426956b9f6637e63ddf4d341b0b
Red-Ducky <galaxy2yt2.0@gmail.com>
2026-03-29 12:55:15 +0200

    Update payload.ps1

--- Backgroound-prank/payload.ps1
+++ Backgroound-prank/payload.ps1
@@ -17,6 +17,66 @@ Add-Type -AssemblyName System.Drawing
 
 Start-Sleep -Milliseconds 500
 
+$ready = $false
+
+$answer1 = [System.Windows.Forms.MessageBox]::Show(
+    "Est-tu vraiment sûr de continuer ?",
+    "Alerte",
+    [System.Windows.Forms.MessageBoxButtons]::YesNo,
+    [System.Windows.Forms.MessageBoxIcon]::Question
+)
+
+$answer2 = $null
+
+if ($answer1 -eq [System.Windows.Forms.DialogResult]::Yes) {
+    $answer2 = [System.Windows.Forms.MessageBox]::Show(
+        "Vraiment ?",
+        "?",
+        [System.Windows.Forms.MessageBoxButtons]::YesNo,
+        [System.Windows.Forms.MessageBoxIcon]::Question
+    )
+}
+if ($answer1 -ne [System.Windows.Forms.DialogResult]::Yes) {
+    [System.Windows.Forms.MessageBox]::Show(
+        "T'es sérieux ou quoi ? Bon vu que tu m'as énervé on va dire que tu voulais bien ! Admire ça !",
+        "Peureux !",
+        [System.Windows.Forms.MessageBoxButtons]::OK,
+        [System.Windows.Forms.MessageBoxIcon]::Question
+    )
+    $ready = $true
+}
+
+if ($ready -eq $false) {
+    $count = 1
+
+    while ($count -le 5 -and $ready -eq $false) {
+        if ($answer2 -eq [System.Windows.Forms.DialogResult]::Yes) {
+        
+            $count++
+            $message = -join (1..$count | ForEach-Object { "?" })
+            
+            $answer2 = [System.Windows.Forms.MessageBox]::Show(
+                "Vraiment $message",
+                "$message",
+                [System.Windows.Forms.MessageBoxButtons]::YesNo,
+                [System.Windows.Forms.MessageBoxIcon]::Question
+            )
+
+            if ($answer2 -ne [System.Windows.Forms.DialogResult]::Yes) {
+                [System.Windows.Forms.MessageBox]::Show(
+                    "T'es sérieux ou quoi ? Bon vu que tu m'as énervé on va dire que tu voulais bien ! Admire ça !",
+                    "Peureux !",
+                    [System.Windows.Forms.MessageBoxButtons]::OK,
+                    [System.Windows.Forms.MessageBoxIcon]::Warning
+                )
+                $ready = $true
+            }
+        }
+    }
+}
+
+Start-Sleep -Milliseconds 300
+
 $path = "$env:TEMP\screenshot.png"
 
 $bounds = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
@@ -50,5 +110,12 @@ Set-ItemProperty -Path $regPath -Name "HideIcons" -Value 1
 
 Stop-Process -Name explorer -Force
 Start-Process explorer
-Start-Sleep -Milliseconds 300
+
+[System.Windows.Forms.MessageBox]::Show(
+    "Félicitations ! Tu viens de détruire ton ordinateur... Amuses-toi bien !",
+    "HAHA",
+    [System.Windows.Forms.MessageBoxButtons]::OK,
+    [System.Windows.Forms.MessageBoxIcon]::Warning
+)
+
 exit
